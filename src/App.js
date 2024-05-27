19c1f01354828a3e13c6d1991ff818943f3b5a67
AzureAD\KennethJohnMatos <kmatos@yotpo.com>
2024-05-27 19:16:25 +0800

    updated product id

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   let InstanceId_ReviewsTab = "";
 
   //Product ID
-  let ProductId = "7141519130729";
+  let ProductId = "7010393096297";
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";
